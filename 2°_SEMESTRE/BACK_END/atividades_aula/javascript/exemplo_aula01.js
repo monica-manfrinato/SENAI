1f340f514784605e72adb992f38bae37f200525e
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2025-08-11 15:25:22 -0300

    adiciona novo exemplo de código no arquivo exemplos_aula01.js

--- 2°_SEMESTRE/BACK_END/atividades_aula/javascript/exemplo_aula01.js
+++
@@ -1,5 +0,0 @@
-var nome = "joao";
-var nome = "maria";
-console.log(nome); // Output: maria
-var nome = "pedro";
-console.log(nome); // Output: pedro
---
+++ 2°_SEMESTRE/BACK_END/atividades_aula/javascript/exemplos_aula01.js
@@ -0,0 +1,141 @@
+// TESTE DO VAR
+
+//var nome = "joao";
+// var nome = "maria";
+// console.log(nome); // Output: maria
+// var nome = "pedro";
+// console.log(nome); // Output: pedro
+
+
+
+
+
+
+// TESTE DO LET
+
+// let idade = 20;
+// idade = 30;
+// console.log(idade); // Output: 30
+
+
+
+
+
+
+// TESTE DO CONST
+
+// const pi = 3.14;
+// console.log(pi); // Output: 3.14
+// pi = 3.14159; // Isso causaria um erro, pois não podemos reatribuir uma constante
+
+
+
+
+
+
+// TESTE DO camelCase
+
+// let nomeCompleto = "Celso Rodrigo Giusti";
+// console.log(nomeCompleto);
+
+
+
+
+
+//TIPOS DE DADOS
+
+// let nome = "Celso"; // String
+// let idade = 20; // Number
+// let altura = 1.75; // Number (float)
+// let isEstudante = true; // Boolean
+// let endereco = null;    // Null
+// let telefone; // Undefined
+
+// console.log(typeof nome); // Output: string
+// console.log(typeof idade);  // Output: number
+// console.log(typeof altura); // Output: number
+// console.log(typeof isEstudante); // Output: boolean
+// console.log(typeof endereco); // Output: object (null é considerado um objeto em JavaScript)
+// console.log(typeof telefone); // Output: undefined
+
+
+
+
+
+
+
+//USO DE LISTAS E OBJETOS, MOSTRANDO COMO ACESSAR UM VALOR SÓ 
+
+// let lista = ['maçã', 'banana', 'laranja'];
+// let pessoa = {nome: 'Celso', idade: 20};
+
+// console.log(lista[0]); // Output: ['maçã']
+// console.log(pessoa.nome, pessoa.idade); // para exibir o nome e a idade do objeto pessoa, colocma-se um ponto entre o nome do objeto e o atributo que se quer acessar. Output: Celso 20
+// console.log(pessoa); // Output: { nome: 'Celso', idade: 20 }
+
+
+
+
+
+
+//USO DA FUNÇÃO SAUDAÇÃO
+
+// function saudacao(nome)
+//  { 
+//      return `Olá, ${nome}!`; 
+//  } 
+//  console.log(saudacao('Celso')); // Output: Olá, ${nome}!
+
+
+
+
+
+//USO DE IF E ELSE
+
+// let idade = 18;
+
+// if (idade >= 18) {
+//     console.log("Você é maior de idade.");
+// } else {
+//     console.log("Você é menor de idade.");
+// }
+
+
+
+
+
+
+//USO DO IF, ELSE IF E ELSE
+
+// let nota = 70;
+
+// if (nota >= 90) {
+//     console.log("Aprovado com A");
+// }else if (nota >= 80) {
+//     console.log("Aprovado com B");
+// }else if (nota >= 70) {
+//     console.log("Aprovado com C");
+// } else {
+//     console.log("Reprovado");
+// }
+
+
+
+
+
+//USO DO SWITCH
+
+// let dia = 3;
+// switch (dia) {
+//     case 1:
+//         console.log("Domingo");
+//         break;
+//     case 2:
+//         console.log("Segunda-feira");
+//         break;
+//     case 3:
+//         console.log("Terça-feira");
+//         break;
+//     default:
+//         console.log("Dia inválido");
+// }
