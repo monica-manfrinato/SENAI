bba27c2d09b289a4c66d90d122c699ccaf5af5c3
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2025-09-01 10:59:08 -0300

    EXEMPLO PROFESSOR

--- 2°_SEMESTRE/BACK_END/atividades_avaliativas/lista_de_exercicios_funcao_monica.js
+++ 2°_SEMESTRE/BACK_END/atividades_avaliativas/lista_de_exercicios_funcao_monica.js
@@ -100,6 +100,22 @@
 // }
 
 
+// EXEMPLO PROFESSOR
+
+function dobro(numero){
+    return numero*2
+}
+
+function aplicarCallback(array, callback){
+    let listaDobro = []
+    for (let i = 0; i < array.length; i++){
+        listaDobro.push(callback(array[i]))
+    }
+    return listaDobro
+}
+
+console.log(aplicarCallback([2,4,6,8,10],dobro))
+
 
 
 // EXERCÍCIO FUNÇÃO RECURSIVA COM 1 AO 10 EM ORDEM DECRESCENTE
