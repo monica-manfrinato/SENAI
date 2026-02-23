7c6750a54e1426d4afebb2153491e0f38f9aadee
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2026-02-23 15:57:33 -0300

    aula mobile e iot

--- 3°_SEMESTRE/MOBILE/exemplos/react_native/first-app/src/App.js
+++ 3°_SEMESTRE/MOBILE/exemplos/react_native/first-app/src/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Exemplo03 from './jsx_examples/exemplo_03_condicionais';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Exemplo03/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f6e0ff',
+    backgroundColor: '#fff0cd',
     alignItems: 'center',
     justifyContent: 'center',
   },
