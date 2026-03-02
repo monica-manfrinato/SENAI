73074ab10364ae3acd137c118361497ee5294ad6
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2026-03-02 15:47:56 -0300

    exercicio semaforo

--- 3°_SEMESTRE/IOT/exercicios/exercicio_semaforo.py
+++ 3°_SEMESTRE/IOT/exercicios/exercicio_semaforo.py
@@ -9,10 +9,39 @@ ledVermelho.value(0)
 ledAmarelo.value(0)
 ledVerde.value(0)
 
-#while(True):
+#forma 1:
 while True:
-    ledVermelho.toggle() sleep(1) ledVerde.toggle() sleep(1)
-    ledAmarelo.toggle()
+    #Semáforo ABRIU
+    ledVerde.value(1)
+    ledAmarelo.value(0)
+    sleep(3)
+   
+    #Semáforo ATENÇÃO
+    ledVerde.value(0)
+    ledAmarelo.value(1)
     sleep(1)
+   
+    #Semáforo FECHADO
+    ledAmarelo.value(0)
+    ledVermelho.value(1)
+    sleep(2)
+    
+    
+#forma 2:
+#ordem = [(ledVerde,3),(ledAmarelo, 1),(ledVermelho,2)]
 
+#while True:
+    #for led, tempo in ordem:
+        #led.value(1)
+        #sleep(tempo)
+        #led.value(0)
+    
+#forma 3:
+    #semaforo_caboVerde = [Pin(16, Pin.OUT),3, Pin(18, Pin.OUT), Pin(17, Pin.OUT)]
+#tempos = [3,2,1]
 
+#while True:
+    #for i in range(len(semoforo_caboVerde)):
+       #semoforo_caboVerde[i].toggle()
+        #sleep(tempos[i])
+        #semoforo_caboVerde[i].toggle()
