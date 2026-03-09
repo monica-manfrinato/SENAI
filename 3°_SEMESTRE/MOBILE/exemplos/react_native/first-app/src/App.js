285251abd927ca018e06494edad0dbbf73462322
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2026-03-09 16:02:55 -0300

    aula IOT e mobile

--- 3°_SEMESTRE/MOBILE/exemplos/react_native/first-app/src/App.js
+++ 3°_SEMESTRE/MOBILE/exemplos/react_native/first-app/src/App.js
@@ -1,18 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Exemplo03 from './jsx_examples/exemplo_03_condicionais';
-import Exemplo04 from './jsx_examples/exemplo_04_listas';
+import { StyleSheet, View } from 'react-native';
 
-import Lista01 from './exercicios/lista_01_monica';
-import Lista02 from './exercicios/lista_02_monica';
-import Lista03 from './exercicios/lista_03_monica';
 
+import ViewExample04 from './basic_components/view_example04';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Lista02/>
+      <ViewExample04/>
       <StatusBar style="auto" />
     </View>
   );
