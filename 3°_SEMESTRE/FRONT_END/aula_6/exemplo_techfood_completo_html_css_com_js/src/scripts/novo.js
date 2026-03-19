db0088fec9c19821dfe066f492e816dd332946f2
Mônica Cotrim Manfrinato <monica.manfrido@aluno.senai.br>
2026-03-19 16:08:21 -0300

    aula HTML

--- 3°_SEMESTRE/FRONT_END/aula_6/exemplo_techfood_completo_html_css_com_js/src/scripts/novo.js
+++ 3°_SEMESTRE/FRONT_END/aula_6/exemplo_techfood_completo_html_css_com_js/src/scripts/novo.js
@@ -6,7 +6,8 @@ const tituloNhoque = document.querySelector('#card_nhoque h3') //pega o elemento
 
 const botoesCompra = document.querySelectorAll(".btn_pedido") //chama vários elementos selectorAll
 
-const terceiroCard = document.querySelector('.card:nth-child(2)') //exatamente como é no css, o pseudoseletor é esse q usa :, NÃO PODE TER ESPAÇO ENTRE AS PALAVRAS E O :
+const terceiroCard = document.querySelector('.card:nth-child(2)') //exatamente como é no css, o pseudoseletor é esse q usa :, NÃO PODE TER ESPAÇO ENTRE AS PALAVRAS E O : 
+//O CARD JÁ É O FILHO, E O NÚMERO Q VAI NO PARENTESES É A POSIÇÃO DELE DENTRO DO PAI
 
 const data = new Date()
 const hora = data.getHours()
@@ -28,6 +29,9 @@ console.log('3. Terceiro card de uma class', terceiroCard)
 saudacao.textContent = hora < 18 && hora > 12 ? 'Bem vindo! boa tarde!' : hora > 18 && hora < 24 ? 'Bem vindo! boa noite!' : 'Bem vindo! Bom dia!'
 
 seuNome.innerHTML="<strong>Meu nome é Celso</strong>" //TEM QUE SER TUDO ENTRE ASPAS
+//INNER HTML é perigoso pq possibilita a ocorrência de HTML INJECTION
+
+//QUANDO QUER ALTERAR UMA CARACTERÍSTICA ESPECÍFICA, NESSE CASO O SRC E O ALT:
 
 // imagem_lasanha.alt = 'Produto esgotado' //muda o alt da imagem
 // imagem_lasanha.src = './src/images/esgotado.jpg' //muda o caminho da imagem
---
+++ 3°_SEMESTRE/FRONT_END/avaliações/atv_pratica_aula_06/src/scripts/script_aula06_monica.js
@@ -0,0 +1,38 @@
+const contador = document.getElementById('contador')
+const botao = document.getElementById('btn-curtir')
+let curtidas = 0
+
+botao.addEventListener("click", () =>{
+    curtidas++
+    contador.textContent = curtidas
+})
+
+
+
+// Monitoramento (Input): Sempre que o usuário digitar no campo de texto, o parágrafo de "Preview" deve mostrar o texto em tempo real.
+
+const campoTexto = document.getElementById('campo-texto')
+const preview = document.getElementById('preview-texto')
+
+campoTexto.addEventListener('input', () =>{
+    preview.textContent = `Digitando: ${campoTexto.value}`
+})
+
+
+// Sensores (Mouse): Ao entrar com o mouse na caixa de cor, mude-a para azul. Ao sair, ela deve voltar à cor original.
+
+const caixaCor = document.getElementById('caixa-cor')
+
+caixaCor.addEventListener("mouseover", ()=> {
+    caixaCor.style.backgroundColor = '#0758a9'
+})
+
+caixaCor.addEventListener("mouseout", ()=> {
+    caixaCor.style.backgroundColor = '#95a5a6'
+})
+
+
+// Desafio Extra (Reset): Crie um botão (ou use uma tecla) que limpe o input e zera o contador ao mesmo tempo.
+
+
+const botaoReset = document.getElementById('caixa-cor')
